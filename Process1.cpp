0575f08cb7b4996ba74c26e1a656c425897ca099
root <root@LAPTOP-0OMKD2DL.localdomain>
2025-11-07 16:30:33 -0500

    first commit

---
+++ Process1.cpp
@@ -0,0 +1,47 @@
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream> 
+#include <cstdlib>
+#include <csignal>
+using namespace std;
+
+void process1(void){
+    int counter = 0; // counter variable
+
+    while(true){ // forever loop
+        counter++;  // increment
+        if(counter % 3 == 0){
+        std::cout << "Cycle number:" << counter << " - " << counter <<" is a multiple of \n";
+        }
+        else{
+             std::cout << "Cycle number " << counter << "\n";
+        }
+         usleep(500000); // sleep for 0.5 seconds
+    }
+}
+
+
+int main(void){
+    signal(SIGTERM, [](int){ 
+        cout << "\nProcess1 PID: " << getpid() << endl; 
+        exit(0); 
+    });
+    signal(SIGINT, [](int){ 
+        cout << "\nProcess1 PID: " << getpid() << endl; 
+        exit(0); 
+    });
+
+
+    pid_t pid = fork();
+
+    if( pid == -1){
+        exit(EXIT_FAILURE);
+    }
+    else if( pid > 0){
+        process1();
+    }
+    else{
+        system("exec ./Process2");
+    }
+}
---
+++ Process2.cpp
@@ -0,0 +1,48 @@
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream> 
+#include <cstdlib>
+#include <csignal>
+using namespace std;
+
+void process2(void){
+    int counter = 0; // counter variable
+    int cycle = 0; // cycle counter/
+
+    while(true){ // forever loop
+
+        counter--;  // increment
+
+        if(counter % 3 == 0){
+        std::cout << "Cycle number:" << (cycle + 1) << " - " << counter <<" is a multiple of 3\n";
+        }
+        else{
+             std::cout << "Cycle number " << cycle << "\n";
+        }
+        cycle++;
+        usleep(500000); // sleep for 0.5 seconds
+    }
+}
+int main(void){
+    signal(SIGTERM, [](int){ 
+        cout << "\nProcess2 PID: " << getpid() << endl; 
+        exit(0); 
+    });
+    signal(SIGINT, [](int){ 
+        cout << "\nProcess2 PID: " << getpid() << endl; 
+        exit(0); 
+    });
+
+    pid_t pid = fork();
+
+    if( pid == -1){
+        exit(EXIT_FAILURE);
+    }
+    else if( pid > 0){
+        process2();
+     }
+     else{
+        return 0;
+     }
+}
